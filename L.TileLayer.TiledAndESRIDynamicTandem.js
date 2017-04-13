03485b2ef216f68d6a34c55bcecbae9bcf8749c0
Gregor Allensworth <gregor@greeninfo.org>
2017-04-13 09:13:08 -0700

    Documentation

---
+++ L.TileLayer.TiledAndESRIDynamicTandem.js
@@ -0,0 +1,62 @@
+/*
+ * L.TileLayer.TiledAndESRIDynamicTandem
+ *
+ * @summary A layer which switches between a tile layer and a ESRI DynamicMapLayer at a given zoom level
+ * @author GreenInfo Network, Gregor Allensworth
+ * @link https://github.com/GreenInfo-Network/L.TileLayer.TiledAndESRIDynamicTandem
+ */
+L.TileLayer.TiledAndESRIDynamicTandem = L.Class.extend({
+    includes: L.Mixin.Events,
+    options: {
+        detailZoomLevel: 14,
+        opacity: 1.0,
+    },
+    initialize: function (tileurl, dynamicurl, options) {
+        options = L.setOptions(this, options);
+
+        // create our tile layer and our dynamic layer
+        // all options are passed as-given to the TileLayer
+        // DynamicMapLayer only gets a few
+        this.tile_layer = L.tileLayer(tileurl, options);
+        this.dynamic_layer = L.esri.dynamicMapLayer(L.extend({ url: dynamicurl }, options));
+    },
+    onAdd: function (map) {
+        // added to map: set whichever of our sublayers should be visible right now, then start listening for zoom changes
+        this._map = map;
+        this.dealWithZoomLevelChange();
+        this._map.on('zoomend', this.dealWithZoomLevelChange, this);
+    },
+    onRemove: function (map) {
+        // removed from map: stop watching the zoom-change, remove our layers from the map
+        this._map.off('zoomend', this.dealWithZoomLevelChange, this);
+        if (this._map.hasLayer(this.dynamic_layer)) this._map.removeLayer(this.dynamic_layer);
+        if (this._map.hasLayer(this.tile_layer)) this._map.removeLayer(this.tile_layer);
+        this._map = undefined;
+    },
+    dealWithZoomLevelChange: function () {
+        if (this._map.getZoom() >= this.options.detailZoomLevel) {
+            if (this._map.hasLayer(this.tile_layer)) this._map.removeLayer(this.tile_layer);
+            if (! this._map.hasLayer(this.dynamic_layer)) this._map.addLayer(this.dynamic_layer);
+
+            try {
+                this.dynamic_layer.setOpacity(this.options.opacity);
+            } catch (e) {}
+        }
+        else {
+            if (! this._map.hasLayer(this.tile_layer)) this._map.addLayer(this.tile_layer);
+            if (this._map.hasLayer(this.dynamic_layer)) this._map.removeLayer(this.dynamic_layer);
+
+            this.tile_layer.setOpacity(this.options.opacity);
+        }
+    },
+    setOpacity: function (newopacity) {
+        this.options.opacity = newopacity;
+
+        // don't just call setOpacity() cuz dynamic one can only setOpacity() if it's in the map right now
+        // pretend we need an update, toggling the appropriate layer and setting opacity as needed
+        if (this._map) this.dealWithZoomLevelChange();
+    },
+});
+L.tiledAndESRIDynamicTandem = function (tileurl, dynamicurl, options) {
+    return new L.TileLayer.TiledAndESRIDynamicTandem(tileurl, dynamicurl, options);
+};
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # L.TileLayer.TiledAndESRIDynamicTandem
-A layer which switches between a tile layer and a ESRI DynamicMapLayer at a given zoom level
+
+A layer which switches between a tile layer and a ESRI DynamicMapLayer at a given zoom level.
+
+This can be useful if your ArcGIS tile service (great for performance, consumes disk space) only goes down to specific depths, but your dynamic data service (slower performance, no creeping disk consumption) can handle the closer-up rendering just fine.
+
+[See the demo here](https://greeninfo-network.github.io/L.TileLayer.TiledAndESRIDynamicTandem/)
+
+Yes, it's sort of a niche need, but it was a real need and you may find it useful too. :)
+
+
+## Constructor and Options
+
+*Quick-start example*
+
+```
+var switcher = L.tiledAndESRIDynamicTandem(
+    // two URLs: tile service and dynamic service
+    "https://gis1.usgs.gov/arcgis/rest/services/PADUS1_4/Fine_Agency_Level/MapServer/tile/{z}/{y}/{x}",
+    "https://gis1.usgs.gov/arcgis/rest/services/PADUS1_4/Fine_Agency_Level_ZoomLevel13_20/MapServer",
+    // options are passed to both layers, some are applicable to the tandem itself
+    {
+        opacity: 0.5,
+        attribution: '<a target="_blank" href="https://gapanalysis.usgs.gov/padus/">Protected Areas Database of the United States (PAD-US)</a>',
+        detailZoomLevel: 14  // level 14+ use dynamic, <=13 use tiled
+    }
+);
+MAP.addLayer(switcher);
+```
+
+* First parameter is an URL template for the tile service.
+* Second parameter is the URL of the ArcGIS DynamicMapService. (This may or may not be similar to that of the tile service.)
+* Third is a dictionary of options.
+	* Options are passed as-given to both of the layers.
+	* Options affecting the tandem behavior are as follows.
+	    * *detailZoomLevel* -- At this zoom level and higher/closer the dynamic service will be used. At levels further out, the tile service will be used.
+
+## Bugs, Features, Leaflet 1.0
+
+This was designed to meet a specific need, so may not suit your specific use case without some further modifications.
+
+This class will not work with Leaflet 1.x This was designed for ESRI-Leaflet adapter 1.x for Leaflet 0.7, whereas ESRI-Leaflet for 1.0 is a 2.x version with a different API. There are no immediate plans to create a Leaflet 1.x version, but if you're interested feel free to contact us.
